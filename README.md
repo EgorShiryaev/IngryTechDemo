eb033af5b68d2d4cd3c9741b29b94ffd5ae08bd6
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-10-30 21:56:52 +0300

    Инициализация проекта flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# ingry_tech
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ingry_tech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ingry_tech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
