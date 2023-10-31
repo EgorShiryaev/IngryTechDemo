8c6a42ed5ac7c8f9f68229abfc5ea76cafdc9ddd
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-11-01 00:59:25 +0300

    Реализована поисковая строка

--- lib/presentation/themes/app_theme.dart
+++ lib/presentation/themes/app_theme.dart
@@ -5,6 +5,8 @@ import 'rounded_navigation_bar_theme_data.dart';
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
+      scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+      fontFamily: 'Roboto',
       navigationBarTheme: const RoundedNavigationBarThemeData(
         height: 90,
         backgroundColor: Color(0xFFE1E000),
@@ -20,6 +22,24 @@ class AppTheme {
           top: Radius.circular(15),
           bottom: Radius.circular(40),
         ),
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFFFFFFFF),
+        elevation: 0,
+      ),
+      searchBarTheme: const SearchBarThemeData(
+        elevation: MaterialStatePropertyAll(0),
+        backgroundColor: MaterialStatePropertyAll(Color(0xFFF5F5F5)),
+        constraints: BoxConstraints.expand(height: 40),
+        shape: MaterialStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+        ),
+        hintStyle: MaterialStatePropertyAll(
+          TextStyle(fontSize: 14, color: Color(0xFF8A8E8F)),
+        ),
+        padding: MaterialStatePropertyAll(EdgeInsets.symmetric(horizontal: 12)),
       ),
     );
   }
