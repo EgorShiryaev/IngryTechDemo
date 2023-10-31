4fc93394c0a1ab2735b0155935d6f82c32c6ee64
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-10-31 20:55:21 +0300

    Реализован NavigationBarNotifier для хранения состояния панели навигации

---
+++ lib/presentation/controllers/navigation_bar_notifier.dart
@@ -0,0 +1,16 @@
+import 'package:riverpod/riverpod.dart';
+
+final navigationBarNotifierProvider =
+    NotifierProvider<NavigationBarNotifier, int>(NavigationBarNotifier.new);
+
+class NavigationBarNotifier extends Notifier<int> {
+  @override
+  int build() {
+    return 0;
+  }
+
+  // ignore: use_setters_to_change_properties
+  void changeIndex(int newIndex) {
+    state = newIndex;
+  }
+}
