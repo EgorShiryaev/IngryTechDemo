f1c18e25eca6dd5e6f0f22c84e7bb1ba279232a1
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-10-31 23:29:59 +0300

    Изменено имя раздела в NavigationBarIcon

--- lib/core/enums/navigation_bar_icon.dart
+++ lib/core/enums/navigation_bar_icon.dart
@@ -1,9 +1,9 @@
-enum NavigationBarIcon { map, list, filter, user }
+enum NavigationBarIcon { map, list, filter, profile }
 
 extension NavigationBarIconEx on NavigationBarIcon {
   String get fullPath => 'assets/icons/$name.svg';
 
   bool get isFirst => this == NavigationBarIcon.map;
 
-  bool get isLast => this == NavigationBarIcon.user;
+  bool get isLast => this == NavigationBarIcon.profile;
 }
