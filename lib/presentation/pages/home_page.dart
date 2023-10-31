9b91bfe1b35d9b38f538ba69e5a92c93e3560be7
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-10-31 23:00:22 +0300

    Добавлены страницы для элементов панели навишации

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../widgets/navigation_stack.dart';
 import '../widgets/rounded_navigation_bar/rounded_navigation_bar.dart';
 
-import 'home_tab_page.dart';
-
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return const Scaffold(
-      body: HomeTabPage(),
+      body: NavigationStack(),
       bottomNavigationBar: RoundedNavigationBar(),
     );
   }
