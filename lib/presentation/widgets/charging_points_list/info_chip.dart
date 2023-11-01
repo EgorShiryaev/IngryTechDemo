81167b6e2db1c0bfc666ac02fc1238373a85113d
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-11-01 17:29:26 +0300

    Реализованы плашки с данными для карточки ЭЗС

---
+++ lib/presentation/widgets/charging_points_list/info_chip.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+
+class InfoChip extends StatelessWidget {
+  final Color backgroundColor;
+  final String label;
+  final Color labelColor;
+  const InfoChip({
+    super.key,
+    required this.backgroundColor,
+    required this.label,
+    required this.labelColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+        color: backgroundColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 10,
+            color: labelColor,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
